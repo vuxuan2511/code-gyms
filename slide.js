54aa85183aad19eb65d0fb1de208e4a7ff5a048a
vuxuan2511 <vuxuan183@gmail.com>
2022-12-28 14:30:31 +0700

    fix bug

---
+++ slide.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.container-slide-to-show ').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        speed: 300,
+        autoplaySpeed: 2000,
+        slidesToScroll: 1,
+    });
+});
